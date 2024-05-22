a7bf094d931fd3fef1542b45027994c467715a81
Will1v <24796480+Will1v@users.noreply.github.com>
2024-05-22 22:06:42 +0100

    Progress on the FH. - Request all exchanges to have a whitelist of supported exchanges - Change websocket message to only request one pair on one exchange (free API Key doesn't allow mutli-subscribe - Clean logger - Implementation of exchanges and order books. Order books record a history of all ticks at each update - Loop over a list of pairs and exchanges to instantiate one FH per (pair, exchange) set. Will require multithreading (TODO)

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 import os
 from dotenv import load_dotenv
 import yaml
-
-# Load environment variables from the .env file
-load_dotenv('config/secrets.env')
-
-# Load general configuration from the config.yml file
-with open('config/config.yml', 'r') as file:
-    config = yaml.safe_load(file)
-
-# Import modules
-from feed_handler.feed_handler import FeedHandler
-# from database.db_manager import DatabaseManager
+from config import config, passwords
+from logger import get_logger
+from crypto_arb_finder.components.feed_handler import FeedHandler
 from web_gui.app import start_web_gui
 
+logger = get_logger(__name__)
+
+
 def main():
-    # Access configuration settings
-    cc_api_key = os.getenv('CRYPTOCOMPARE_API_KEY')
-    db_password = os.getenv('DB_PASSWORD')
-    
+    logger.info("Starting Crypto Arb Opportunities Finder")
     ## Initialize database
-    #db_config = config['database']
-    #database = DatabaseManager(db_config['host'], db_config['port'], db_config['name'], db_password)
-    
+    # db_config = config['database']
+    # database = DatabaseManager(db_config['host'], db_config['port'], db_config['name'], db_password)
+
+    exchanges_to_monitor = ["Binance", "Coinbase"]
+    pairs_to_monitor = [("BTC", "ETH"), ("BTC", "USD"), ("BTC", "GBP")]
+
     # Initialize feed handler
-    feed_config = config['feed_handler']
-    feed_handler = FeedHandler(feed_config['feed_url'], cc_api_key)
-    feed_handler.pull_live_data('BTC', 'USD')
-    
+    feed_config = config.feed_handler
+    for exchange in exchanges_to_monitor:
+        for ccy_1, ccy_2 in pairs_to_monitor:
+            feed_handler = FeedHandler(ccy_1, ccy_2, exchange)
+            feed_handler.pull_live_data()
+
     # Start web GUI
-    #start_web_gui()
+    # start_web_gui()
+
 
 if __name__ == "__main__":
     main()
