b3f86fdeb37d9cc07f6a2af76801e00d646b99e2
Will1v <24796480+Will1v@users.noreply.github.com>
2024-05-27 13:56:31 +0100

    Specify logs location

--- logger/logger.py
+++ logger/logger.py
@@ -1,22 +1,23 @@
 import logging
 import os
 from datetime import datetime
+from crypto_arb_finder.config import config
 
 
 def configure_logger():
     # Define the logging format
     log_format = "%(asctime)s [%(levelname)s] - %(message)s - %(filename)s:%(lineno)d"
-    log_file_timestamp = datetime.now().strftime(
-        "%Y%m%d"
-    )  # datetime.now().strftime('%Y%m%d-%H%M%S')
+    log_file_timestamp = datetime.now().strftime("%Y%m%d")
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format=log_format,
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[
             logging.FileHandler(
-                f"crypto_arb_finder-{log_file_timestamp}.log"
-                # f"crypto_arb_finder-{os.getpid()}-{datetime.now().strftime('%Y%m%d-%H%M%S')}.log"
+                # f"crypto_arb_finder-{log_file_timestamp}.log",
+                os.path.expanduser(config.logger.logs_path)
+                + f"crypto_arb_finder-{os.getpid()}-{datetime.now().strftime('%Y%m%d-%H%M%S')}.log",
+                mode="w",
             ),  # Log to a file
             logging.StreamHandler(),  # Log to console
         ],
