2dca53fe0a44a997c8e9eec05eb3af7b14dd3e42
Will1v <24796480+Will1v@users.noreply.github.com>
2024-06-20 00:17:30 +0100

    Coinbase feed handler support for full order book

--- market/order_book.py
+++ market/order_book.py
@@ -1,5 +1,6 @@
 from logger import get_logger
 from datetime import datetime
+import pytz
 from database import db_helper
 import time
 import threading
@@ -20,7 +21,11 @@ class OrderBook(threading.Thread):
         self._bids = SortedDict(lambda x: -x)
         self._asks = SortedDict()
         self._bid_ask_history = []
-        self.last_update = datetime.now()
+        self._last_best_bid = None
+        self._last_best_bid_q = None
+        self._last_best_ask = None
+        self._last_best_ask_q = None
+        self.last_update = datetime.now(pytz.UTC)
         self.db_queue = queue.Queue()
 
         # Multithreading management
@@ -46,17 +51,30 @@ class OrderBook(threading.Thread):
         return self._exchange
     
     def register_best_bid_offer(self) -> None: 
-            best_bid, best_bid_q = next(self._bids.items())
-            best_ask, best_ask_q = next(self._asks.items())
+        best_bid, best_bid_q = next(iter(self._bids.items()), (None, None))
+        best_ask, best_ask_q = next(iter(self._asks.items()), (None, None))
+        if (self._last_best_bid, self._last_best_bid_q, self._last_best_ask, self._last_best_ask_q) != (best_bid, best_bid_q, best_ask, best_ask_q):
             self._bid_ask_history.append(
-            {
-                "event_time": self.last_update,
-                "bid_q": best_bid_q,
-                "bid": best_bid,
-                "ask": best_ask,
-                "ask_q": best_ask_q,
-            }
-        )
+                {
+                    "event_time": self.last_update,
+                    "bid_q": best_bid_q,
+                    "bid": best_bid,
+                    "ask": best_ask,
+                    "ask_q": best_ask_q,
+                }
+            )
+            if self._last_best_bid != best_bid:
+                logger.debug(f"self._last_best_bid {self._last_best_bid} != best_bid {best_bid}")
+            if self._last_best_bid_q != best_bid_q:
+                logger.debug(f"self._last_best_bid_q {self._last_best_bid_q} != best_bid_q {best_bid_q}")
+            if self._last_best_ask != best_ask:
+                logger.debug(f"self._last_best_ask {self._last_best_ask} != best_ask {best_ask}")
+            if self._last_best_ask_q != best_ask_q:
+                logger.debug(f"self._last_best_ask_q {self._last_best_ask_q} != best_ask_q {best_ask_q}")
+            logger.debug(f"register_best_bid_offer: self._bid_ask_history now size {len(self._bid_ask_history)}")
+            self._last_best_bid, self._last_best_bid_q, self._last_best_ask, self._last_best_ask_q = best_bid, best_bid_q, best_ask, best_ask_q
+        else:
+            logger.debug("No change to BBO")
     
     def set_bid(self, bid: float, bid_q: float, event_time: datetime):
         self.register_tick(bid_ask='bid', price=bid, quantity=bid_q, event_time=event_time)
@@ -67,8 +85,9 @@ class OrderBook(threading.Thread):
     def register_tick(self, bid_ask: Literal['bid', 'ask'], price: float, quantity: float, event_time: datetime):
         # If we're on a new event time, save the current BBO
         if event_time > self.last_update:
+            logger.debug(f"register_tick - event_time = {event_time} newer than self.last_update = {self.last_update}")
             self.register_best_bid_offer()
-            self.last_update == event_time
+            self.last_update = event_time
         # New limit or modified quantity on existing limit
         if quantity > 0:
             if bid_ask == 'bid':
@@ -78,69 +97,12 @@ class OrderBook(threading.Thread):
         # Limit gone, to remove
         else:
             if bid_ask == 'bid':
-                del self._bids[price]
+                if price in self._bids:
+                    del self._bids[price]
             else:
-                del self._asks[price] 
+                if price in self._asks:
+                    del self._asks[price] 
         
-
-
-
-    """    
-    def set_bid(self, bid: float, bid_q: float, event_time: datetime):
-        self.set_bid_ask(
-            bid=bid, bid_q=bid_q, ask=self.ask, ask_q=self.ask_q, event_time=event_time
-        )
-
-    def set_ask(self, ask: float, ask_q: float, event_time: datetime):
-        self.set_bid_ask(
-            bid=self.bid, bid_q=self.bid_q, ask=ask, ask_q=ask_q, event_time=event_time
-        )
-
-
-    def set_bid_ask(
-        self,
-        bid: float,
-        bid_q: float,
-        ask: float,
-        ask_q: float,
-        event_time: datetime,
-    ):
-        logger.debug(f"set_bid_ask: bid: {bid_q}@{bid} / ask: {ask_q}@{ask}")
-        with self.lock:
-            if bid:
-                if self._ask and bid >= self._ask:
-                    logger.warning(
-                        f"Crossed book: Setting bid to {bid} (>= ask {self._ask})"
-                    )
-                if bid < 0:
-                    logger.error(f"Can't have a negative bid")
-                if bid_q and bid_q < 0:
-                    logger.error(f"BidQ: {bid_q} is invalid (should be >= 0)")
-            self._bid = bid
-            self._bid_q = bid_q
-            if ask:
-                if self._bid and ask <= self._bid:
-                    logger.warning(
-                        f"Crossed book: Setting ask to {ask} (<= bid {self._bid})"
-                    )
-                if ask < 0:
-                    logger.error(f"ERROR: can't have a negative ask")
-                if ask_q < 0:
-                    logger.error(f"AskQ: {ask_q} is invalid (should be >= 0)")
-            self._ask = ask
-            self._ask_q = ask_q
-            self._bid_ask_history.append(
-                {
-                    "event_time": event_time,
-                    "bid_q": self._bid_q,
-                    "bid": self._bid,
-                    "ask": self._ask,
-                    "ask_q": self._ask_q,
-                }
-            )
-            logger.info(f"New tick: {self}")
-            """
-
     # This fills the queue of updates to dump in DB every 1 second
     def periodic_insertion(self):
         logger.debug(
@@ -192,7 +154,7 @@ class OrderBook(threading.Thread):
                     )
                     for h in data_batch
                 ]
-                logger.debug(f"values = {values}")
+                logger.debug(f"values = {len(values)}")
                 db_conn.cursor().executemany(insert_query, values)
                 db_conn.commit()
                 logger.info(f"{len(data_batch)} entries added to DB")
@@ -207,4 +169,6 @@ class OrderBook(threading.Thread):
 
     # Dunder methods...
     def __str__(self):
-        return f"[OrderBook] [{self.exchange}:{self._ccy_1}/{self._ccy_2}] {self.bid_q}@{self.bid} / {self.ask_q}@{self.ask}"
+        best_bid, best_bid_q = next(iter(self._bids.items()), (None, None))
+        best_ask, best_ask_q = next(iter(self._asks.items()), (None, None))
+        return f"[OrderBook] [{self.exchange}:{self._ccy_1}/{self._ccy_2}] {best_bid_q}@{best_bid} / {best_ask_q}@{best_ask}"
